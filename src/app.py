6c482e57613036846c187e8df2f40a7f3ed4147c
Phillipe Barcelos <phillipe.sa@supermix.com.br>
2019-10-25 11:37:46 -0300

    Include test wifi e os id

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
 import src.common.get_token as auth
+import src.common.wifi_connection as wificon
+import src.common.osidentify as machine_os
 
 def run():          
     auth.TestGet()
+    wificon.Search()
+    machine_os.SearchOS()
